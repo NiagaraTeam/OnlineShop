1d9a531993839bc725ad0b81d48f6b30d8987ae7
Dorota Maliszewska <dmali0@op.pl>
2023-11-16 14:56:23 +0100

    OrderService->bez postmana

--- Application/Dto/Order/OrderDto.cs
+++ Application/Dto/Order/OrderDto.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
+using Domain.Enums;
 
 namespace Application.Dto.Order
 {
     public class OrderDto
     {
-        
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public int CustomerDetailsId { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public ShippingMethod ShippingMethod { get; set; }
+        public ICollection<OrderItemDto> Items { get; set; }
+
     }
 }
--- Application/Dto/Order/OrderItemDto.cs
+++ Application/Dto/Order/OrderItemDto.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Dto.Product;
 
 namespace Application.Dto.Order
 {
     public class OrderItemDto
     {
-        
+        public decimal Quantity { get; set; }
+        public int ProductId;
     }
 }
