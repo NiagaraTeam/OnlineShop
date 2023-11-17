e35faa6885e9a90a5162dd8fa8c207eece045098
Justyna Sadowska <83035@student.pb.edu.pl>
2023-11-17 23:43:04 +0100

    userservice

--- Application/Dto/User/AddressDto.cs
+++ Application/Dto/User/AddressDto.cs
@@ -7,6 +7,6 @@ namespace Application.Dto.User
 {
     public class AddressDto
     {
-        
+        public string Address {get; set;}
     }
 }
--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,44 +1,161 @@
 using Application.Core;
 using Application.Dto.User;
 using Application.Interfaces;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Services
 {
     public class UserService : IUserService
-    {//to ja
-        public Task<Result<object>> AddFavouriteProduct(int userId, int productId)
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserService(
+            DataContext context,
+            IMapper mapper,
+            UserManager<AppUser> userManager
+        )
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+        public async Task<Result<object>> AddFavouriteProduct(string userId, int productId)
+        {
+            var user = await _context.Users.Include(u => u.CustomerDetails).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<Object>.Failure("User not found");
+            }
+
+            if (user.CustomerDetails.FavouriteProducts == null)
+            {
+                user.CustomerDetails.FavouriteProducts = new List<CustomerDetailsProduct>();
+            }
+            if (user.CustomerDetails.FavouriteProducts.Any(fp => fp.ProductId == productId))
+            {
+                return Result<Object>.Failure("Product is already in favourites");
+            }
+
+            user.CustomerDetails.FavouriteProducts.Add(new CustomerDetailsProduct { ProductId = productId });
+
+            if (await _context.SaveChangesAsync() > 0)
+                return Result<object>.Success(null);
+
+            return Result<object>.Failure("Failed adding the product");
         }
 
-        public Task<Result<object>> DeleteAccount(int userId)
+        public async Task<Result<object>> DeleteAccount(string userId)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<Object>.Failure("User not found");
+            }
+
+            _context.Users.Remove(user);
+
+            if (await _context.SaveChangesAsync() > 0)
+                return Result<object>.Success(null);
+
+            return Result<object>.Failure("Failed deleting the account");
         }
 
-        public Task<Result<decimal>> GetUserDiscount(int userId)
+        public async Task<Result<decimal>> GetUserDiscount(string userId)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.Include(u => u.CustomerDetails).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<decimal>.Failure("User not found");
+            }
+            if (user.CustomerDetails == null )
+            {
+                return Result<decimal>.Failure("Discount data not available");
+            }
+
+            return Result<decimal>.Success(user.CustomerDetails.DiscountValue);
         }
 
-        public Task<Result<object>> RemoveFavouriteProduct(int userId, int productId)
+        public async Task<Result<object>> RemoveFavouriteProduct(string userId, int productId)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.Include(u => u.CustomerDetails).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<object>.Failure("User not found");
+            }
+
+            if (user.CustomerDetails.FavouriteProducts == null)
+            {
+                user.CustomerDetails.FavouriteProducts = new List<CustomerDetailsProduct>();
+            }
+
+            var favoriteProduct = user.CustomerDetails.FavouriteProducts.FirstOrDefault(fp => fp.ProductId == productId);
+            if (favoriteProduct != null)
+            {
+                user.CustomerDetails.FavouriteProducts.Remove(favoriteProduct);
+                await _context.SaveChangesAsync();
+            }
+
+            return Result<object>.Success(null);
         }
 
-        public Task<Result<object>> ResetPasswordRequest(int userId)
+        public async Task<Result<object>> ResetPasswordRequest(string userId)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                return Result<Object>.Failure("User not found");
+            }
+
+            var resetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            Console.WriteLine($"Reset token for user {user.Email}: {resetToken}");
+
+            return Result<object>.Success(null);
         }
 
-        public Task<Result<object>> SetUserDiscount(int userId, decimal discountValue)
+        public async Task<Result<object>> SetUserDiscount(string userId, decimal discountValue)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.Include(u => u.CustomerDetails).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<Object>.Failure("User not found");
+            }
+
+            user.CustomerDetails.DiscountValue = discountValue;
+
+            if (await _context.SaveChangesAsync() > 0)
+                return Result<object>.Success(null);
+
+            return Result<object>.Failure("Failed setting user discount");
         }
 
-        public Task<Result<object>> UpdateUserAddress(int userId, AddressDto address)
+        public async Task<Result<object>> UpdateUserAddress(string userId, AddressDto address)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users.Include(u => u.CustomerDetails).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return Result<Object>.Failure("User not found");
+            }
+
+            _mapper.Map(address, user.CustomerDetails.Address);
+
+            if (await _context.SaveChangesAsync() > 0)
+                return Result<object>.Success(null);
+
+            return Result<object>.Failure("Failed updating the address");
         }
     }
 }
