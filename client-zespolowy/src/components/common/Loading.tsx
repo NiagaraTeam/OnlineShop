a10961008237725b5c6d985cd8042dc5bbdbe850
Cezary Stachurski <czareks20006@gmail.com>
2023-11-19 15:22:59 +0100

    spinner size change

--- client-zespolowy/src/components/common/Loading.tsx
+++ client-zespolowy/src/components/common/Loading.tsx
@@ -1,6 +1,9 @@
 const Loading = () => {
     return (
-        <div className="spinner-border" role="status">
+        <div 
+            className="spinner-border" role="status" 
+            style={{width: "4rem", height: "4rem"}}
+        >
             <span className="visually-hidden">Loading...</span>
         </div>
     )
