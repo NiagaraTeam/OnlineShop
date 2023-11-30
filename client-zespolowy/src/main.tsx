7c5e17bf9dd6bf2bd780811cca1cdcc485c8e326
Cezary Stachurski <czareks20006@gmail.com>
2023-11-30 10:27:22 +0100

    fix

--- client-zespolowy/src/main.tsx
+++ client-zespolowy/src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import './app/layout/App.css'
-import "bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'react-toastify/dist/ReactToastify.min.css'
