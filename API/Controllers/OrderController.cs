dc132fe2b0f0b662975c56fec25e2ea7d2a0185a
Cezary Stachurski <czareks20006@gmail.com>
2023-12-02 14:20:28 +0100

    requestloggingmiddleware

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -15,12 +15,14 @@ namespace API.Controllers
         {
             _orderService = orderService;
         }
-        [HttpGet("orders")]
+
+        [HttpGet("orders")] //api/orders
         [Authorize(Roles = StaticUserRoles.ADMIN)] 
         public async Task<IActionResult> GetAllOrders()
         {
             return HandleResult(await _orderService.GetAllOrders());
         }
+
         [HttpPost("orders")] //api/orders
         [Authorize(Roles = StaticUserRoles.CUSTOMER)]
         public async Task<IActionResult> CreateOrder(OrderCreateUpdateDto order)
